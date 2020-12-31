6f9e7d678b9bfcdacd9409ed257f3b2029c66b3e
Jason Solarz <JasonSolarz@gmail.com>
2020-12-31 07:15:26 -0500

    Refactored and added light input validation

--- Easy1/sumOrProduct.js
+++ Easy1/sumOrProduct.js
@@ -1,21 +1,58 @@
 /* Write a program that asks the user to enter an integer greater than 0, then asks whether the user wants to determine the sum or the product of all numbers between 1 and the entered integer, inclusive. */
 
 let readline = require("readline-sync");
+let userInteger;
+let userChoice;
 
-let userInteger = Number(readline.question("Please enter an integer greater than 0: "));
-let userChoice = readline.question('Enter "s" to compute the sum, or "p" to compute the product. ');
+function getUserInteger() {
+  let userInput;
+  let validInput = false;
+  do {
+    userInput = Number(readline.question("Please enter an integer greater than 0: "));
+    if(userInput > 0) {
+      validInput = true;
+    } else {
+      console.log("Please enter a value greater than 0\n");
+    }
+  }while(!validInput);
 
-if(userChoice.toLowerCase() === "s") {
-  let sum = 0;
-  for(let lcv = 1; lcv <= userInteger; lcv++) {
-    sum = sum + lcv;
-  }
-  console.log(`The sum of the integers between 1 and ${userInteger} is ${sum}`);
-} else if(userChoice.toLowerCase() === "p") {
-  let product = 1;
-  for(let lcv = 1; lcv <= userInteger; lcv++) {
-    product = product * lcv;
-  }
-  console.log(`The product of the integers between 1 and ${userInteger} is ${product}`);
+  return userInput;
 }
+
+function getUserChoice() {
+  let userInput;
+  let validInput = false;
+  do {
+    userInput = readline.question('Enter "s" to compute the sum, or "p" to compute the product. ');
+    if(userInput.toLowerCase() === "s" || userInput.toLowerCase() === "p") {
+      validInput = true;
+    } else {
+      console.log(`"${userInput}" is not a valid option. Please choose again.\n`);
+    }
+  }while(!validInput);
+
+  return userInput;
+}
+
+function calculate(num, type) {
+  if(type.toLowerCase() === "s") {
+    let sum = 0;
+    for(let lcv = 1; lcv <= num; lcv++) {
+      sum = sum + lcv;
+    }
+    console.log(`The sum of the integers between 1 and ${num} is ${sum}`);
+  } else if(type.toLowerCase() === "p") {
+    let product = 1;
+    for(let lcv = 1; lcv <= num; lcv++) {
+      product = product * lcv;
+    }
+    console.log(`The product of the integers between 1 and ${num} is ${product}`);
+  }
+}
+
+userInteger = getUserInteger();
+userChoice = getUserChoice();
+calculate(userInteger, userChoice);
+
+
 
