6ed4bdabf45949f84013e05bff20f7815067d63a
Jason Solarz <JasonSolarz@gmail.com>
2020-12-31 05:19:07 -0500

    Added input type of meters or feet

--- Easy1/calcRoomSize.js
+++ Easy1/calcRoomSize.js
@@ -4,15 +4,35 @@ Build a program that asks the user to enter the length and width of a room in me
 Note: 1 square meter == 10.7639 square feet
 
 Do not worry about validating the input at this time. Use the readlineSync.prompt method to collect user input.
+
+Further Exploration
+
+Modify the program so that it asks the user for the input type (meters or feet). Compute for the area accordingly, and log it and its conversion in parentheses.
 */
 
 function calcRoomSize() {
   let readLine = require("readline-sync");
-  let length = Number(readLine.question("Enter the length of the room in meters: "));
-  let width = Number(readLine.question("Enter the width of the room in meters: "));
-  let area = length * width;
-  let areaInFeet = area * 10.7639;
-  console.log(`The area of the room is ${area} square meters (${areaInFeet.toFixed(2)} square feet).`);
+  let length;
+  let width;
+  let areaInMeters;
+  let areaInFeet;
+
+  let inputType = readLine.question("Please select an input type: (meters or feet): ");
+  if(inputType.toLowerCase() === "meters") {
+    length = Number(readLine.question("Enter the length of the room in meters: "));
+    width = Number(readLine.question("Enter the width of the room in meters: "));
+    areaInMeters = length * width;
+    areaInFeet = areaInMeters * 10.7639;
+    console.log(`The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`);
+  } else if(inputType.toLowerCase() === "feet") {
+      length = Number(readLine.question("Enter the length of the room in feet: "));
+      width = Number(readLine.question("Enter the width of the room in meters: "));
+      areaInFeet = length * width;
+      areaInMeters = areaInFeet / 10.7639;
+      console.log(`The area of the room is ${areaInFeet.toFixed(2)} square feet (${areaInMeters.toFixed(2)} square meters).`);
+  }
+
+
 }
 
 calcRoomSize();
