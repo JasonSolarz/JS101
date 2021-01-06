766ff15f3fd0196f41ef52e4cb64c96e762e6200
Jason Solarz <JasonSolarz@gmail.com>
2021-01-06 06:54:29 -0500

    Refactored & ESLint Calculator.js

--- Lesson_2/calculator.js
+++ Lesson_2/calculator.js
@@ -6,31 +6,59 @@
 
 const readline = require("readline-sync");
 
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
 console.log("**********************************");
 console.log("*     WELCOME TO CALCULATOR!     *");
 console.log("**********************************");
 
-console.log("What's the first number?");
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log("What operation would you like to perform?\n1) Add \n2) Subtract \n3) Multiply \n4) Divide");
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question();
+}
+
+prompt("What operation would you like to perform?\n1) Add \n2) Subtract \n3) Multiply \n4) Divide");
 let operation = readline.question();
 
+while (!["1", "2", "3", "4"].includes(operation)) {
+  prompt("Must choose 1, 2, 3 or 4");
+  operation = readline.question();
+}
+
 let output;
-if (operation === "1") {
-  output = Number(number1) + Number(number2);
-} else if (operation === "2") {
-  output = Number(number1) - Number(number2);
-} else if (operation === "3") {
-  output = Number(number1) * Number(number2);
-} else if (operation === "4") {
-  output = Number(number1) / Number(number2);
-} else {
-  console.log("I'm sorry. That operation is not available!");
-  output = 0;
+
+switch (operation) {
+  case "1":
+    output = Number(number1) + Number(number2);
+    break;
+  case "2":
+    output = Number(number1) - Number(number2);
+    break;
+  case "3":
+    output = Number(number1) * Number(number2);
+    break;
+  case "4":
+    output = Number(number1) / Number(number2);
+    break;
+  default:
+    prompt("I'm sorry. That operation is not available!");
 }
 
 console.log(`The result is: ${output}`);
